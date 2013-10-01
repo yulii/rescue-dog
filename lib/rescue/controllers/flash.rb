eda303bcb0d0296fe55b9f1581d725a0a3576722
yulii <yuliinfo@gmail.com>
2013-10-01 19:49:19 +0900

    I18n performance improvement

--- lib/rescue/controllers/flash.rb
+++ lib/rescue/controllers/flash.rb
@@ -4,20 +4,18 @@ module Rescue
     class Flash
 
       def self.message object, key 
-        scope = [:views]
-        scope += object.controller_path.split('/')
-        scope << object.action_name
-        scope << :flash
+        scope = "views.#{object.controller_path.gsub(%r{/}, '.')}.#{object.action_name}.flash"
         text(key, scope)
       end
 
+      # Private Methods
       def self.default key
-        s = I18n.t(key, scope: [:default, :flash], default: '')
+        s = I18n.t("default.flash.#{key}", default: '')
         s.blank? ? nil : s
       end
 
-      def self.text key, scope = []
-        s = I18n.t(key, scope: scope, default: '')
+      def self.text key, scope
+        s = I18n.t("#{scope}.#{key}", default: '')
         s.blank? ? default(key) : s
       end
       private_class_method :default, :text
